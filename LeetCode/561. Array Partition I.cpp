5b45162aa324ac1aeee36aacb2ff85642c093577
hhyvs111 <445622919@qq.com>
2019-01-07 21:45:02 +0800

    继续开始刷题

---
+++ LeetCode/561. Array Partition I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(int i = 0;i < nums.size(); i+=2)
+        {
+            ans += min(nums[i], nums[i+1]);
+        }
+        
+        return ans;
+    }
+};
+
+//看了提示才做出来的，先排序，然后取小的。
