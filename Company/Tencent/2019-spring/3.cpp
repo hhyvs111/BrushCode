16d016e4174bb41588956467c0d9af5705c53e91
hhyvs111 <445622919@qq.com>
2019-04-10 17:11:22 +0800

    test of huawei

--- Company/Tencent/2019-spring/3.cpp
+++ Company/Tencent/2019-spring/3.cpp
@@ -4,33 +4,22 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> monster(n);
-    vector<int> money(n);
+    vector<int> monster(n, 0);
+    vector<int> money(n, 0);
     for(int i = 0;i<n;i++)
     {
-        int tmp;
-        cin>>tmp;
-        monster[i] = tmp;
+        cin>>monster[i];
     }
-    for(int i = 0;i<n;i++)
+    long long cur = 0;
+    int ans = 0;
+    for(int i = 0;i < n;i++)
     {
-        int t;
-        cin>>t;
-        money[i] = t;
-    }
-    int indexMonster = monster[0];
-    int indexMoney = money[0];
-    for(int i = 1;i < n;i++)
-    {
-        for(int j = i+ 1;j < n;j++)
-        {
-              if(indexMonster <= monster[j])
-              {
-                    indexMoney += money[i];
-                    indexMonster += monster[i];
-              }
+        cin >> money[i];
+        if(cur < monster[i]){
+            cur += monster[i];
+            ans += money[i];
         }
     }
-    cout << indexMoney << endl;
+    cout << ans << endl;
     return 0;
 }
